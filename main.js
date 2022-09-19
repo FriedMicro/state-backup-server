2c07756001ceb6e6015bb2ebfdf4e0cb8b3032a5
Lucas Smith <lucas@lucasesmith.com>
2022-09-18 23:09:43 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+import Koa from "koa";
+import bodyParser from "koa-bodyparser";
+import config from "./config.js";
+
+const app = new Koa();
+
+const state = {};
+
+app.use(bodyParser());
+
+app.use(async ctx => {
+    const method = ctx.request.method;
+    if(method === "POST"){
+        state = ctx.request.body.state;
+    } else {
+        ctx.body = JSON.stringify(state);
+    }
+});
+
+app.listen(config.port);
